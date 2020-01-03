91a53f9ed9e648f999de727223a03e4c077df7cf
mrdoomus <mrdoom.official@gmail.com>
2020-01-03 18:02:45 -0500

    master: Fix changed path for provider

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,50 +1,50 @@
 #Executes task #0 but in puppet mode ;)
 exec { 'Update':
-  path    => '/usr/bin/env',
-  command => 'apt-get -y update',
+  command  => 'apt-get -y update',
+  provider => shell,
 }
 exec { 'Get nginx':
-  path    => '/usr/bin/env',
-  command => 'apt-get -y install nginx',
+  command  => 'apt-get -y install nginx',
+  provider => shell,
 }
 exec { 'create data folder':
-  path    => '/usr/bin/env',
-  command => 'mkdir -p /data/',
+  command  => 'mkdir -p /data/',
+  provider => shell,
 }
 exec { 'create web_static folder in data':
-  path    => '/usr/bin/env',
-  command => 'mkdir -p /data/web_static/',
+  command  => 'mkdir -p /data/web_static/',
+  provider => shell,
 }
 exec { 'create releases folder in web_static':
-  path    => '/usr/bin/env',
-  command => 'mkdir -p /data/web_static/releases/',
+  command  => 'mkdir -p /data/web_static/releases/',
+  provider => shell,
 }
 exec { 'create shared folder in web_static':
-  path    => '/usr/bin/env',
-  command => 'mkdir -p /data/web_static/shared/',
+  command  => 'mkdir -p /data/web_static/shared/',
+  provider => shell,
 }
 exec { 'create test folder in releases':
-  path    => '/usr/bin/env',
-  command => 'mkdir -p /data/web_static/releases/test/',
+  command  => 'mkdir -p /data/web_static/releases/test/',
+  provider => shell,
 }
 exec { 'Writes "hope it works" in tests index.html':
-  path    => '/usr/bin/env',
-  command => 'echo "hope it works" > /data/web_static/releases/test/index.html',
+  command  => 'echo "hope it works" > /data/web_static/releases/test/index.html',
+  provider => shell,
 }
 exec { 'creates symbolic link between test and current':
-  path    => '/usr/bin/env',
-  command => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  provider => shell,
 }
 exec { 'changes owner and group to data':
-  path    => '/usr/bin/env',
-  command => 'chown -R ubuntu:ubuntu /data/',
+  command  => 'chown -R ubuntu:ubuntu /data/',
+  provider => shell,
 }
 exec { 'adds alias to location':
-  path    => '/usr/bin/env',
-  command => 'sed -i "/listen 80 default_server;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}"
+  command  => 'sed -i "/listen 80 default_server;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}"
   /etc/nginx/sites-available/default',
+  provider => shell,
 }
 exec { 'restarts nginx service':
-  path    => '/usr/bin/env',
-  command => 'service nginx restart',
+  command  => 'service nginx restart',
+  provider => shell,
 }
