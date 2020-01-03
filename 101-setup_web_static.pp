88c38e499db2630d39a7487a94e1e3b01b90e789
mrdoomus <mrdoom.official@gmail.com>
2020-01-03 17:51:57 -0500

    master: Fix path for puppet commands

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,50 +1,50 @@
 #Executes task #0 but in puppet mode ;)
 exec { 'Update':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'apt-get -y update',
 }
 exec { 'Get nginx':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'apt-get -y install nginx',
 }
 exec { 'create data folder':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'mkdir -p /data/',
 }
 exec { 'create web_static folder in data':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'mkdir -p /data/web_static/',
 }
 exec { 'create releases folder in web_static':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'mkdir -p /data/web_static/releases/',
 }
 exec { 'create shared folder in web_static':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'mkdir -p /data/web_static/shared/',
 }
 exec { 'create test folder in releases':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'mkdir -p /data/web_static/releases/test/',
 }
 exec { 'Writes "hope it works" in tests index.html':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'echo "hope it works" > /data/web_static/releases/test/index.html',
 }
 exec { 'creates symbolic link between test and current':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
 }
 exec { 'changes owner and group to data':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'chown -R ubuntu:ubuntu /data/',
 }
 exec { 'adds alias to location':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'sed -i "/listen 80 default_server;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}"
   /etc/nginx/sites-available/default',
 }
 exec { 'restarts nginx service':
-  path    => '/usr/bin',
+  path    => '/usr/bin/env',
   command => 'service nginx restart',
 }
