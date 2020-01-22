150c14843a58ed4f02e73f6c5e9d4de000155a40
mrdoomus <mrdoom.official@gmail.com>
2020-01-22 09:20:42 -0500

    master: [Fix] Changed function name and encapsulated <ul> in previous <li>

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,7 +7,7 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/cities_by_states')
-def cities_states_list():
+def cities_by_states():
     return render_template(
         '8-cities_by_states.html',
         states=storage.all('State').values())
